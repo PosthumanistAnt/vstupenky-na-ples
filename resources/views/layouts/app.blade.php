453a853bd95825f5e681005e9a04d16424368d2b
Pavel Vašíček <mr.troler@seznam.cz>
2021-01-17 21:39:52 +0100

    Added tailwind and linked it in layouts.app

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vstupenky na ples</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div>
+        {{ $slot }}
+    </div>
+</body>
+</html>
