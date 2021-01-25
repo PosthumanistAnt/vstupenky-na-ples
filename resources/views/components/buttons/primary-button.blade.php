caea1f3281a7ba896c39c7164ca4cb4f01e34bd5
Pavel Vašíček <mr.troler@seznam.cz>
2021-01-25 15:41:24 +0100

    ✨ Added livewireFunction property to buttons

--- resources/views/components/buttons/primary-button.blade.php
+++ resources/views/components/buttons/primary-button.blade.php
@@ -1,5 +1,7 @@
-@props(['text'])
-
+@props([
+    'text',
+    'livewireFunction',
+])
 <div>
-    <button class="p-6 text-5xl xl:text-8xl tracking-widest font-extrabold hover:text-gray-900" wire:click="login"> {{ $text }} </button>
+    <button class="p-6 text-5xl xl:text-8xl tracking-widest font-extrabold hover:text-gray-400" wire:click="{{ $livewireFunction }}"> {{ $text }} </button>
 </div>
--- resources/views/components/buttons/secondary-button.blade.php
+++ resources/views/components/buttons/secondary-button.blade.php
@@ -1,5 +1,8 @@
-@props(['text'])
+@props([
+    'text',
+    'livewireFunction',
+])
 
 <div>
-    <button class="p-6 text-4xl xl:text-7xl tracking-widest font-extrabold hover:text-gray-900" wire:click="register"> {{ $text }} </button>
+    <button class="p-6 text-4xl xl:text-7xl tracking-widest font-extrabold hover:text-gray-400" wire:click="{{ $livewireFunction }}"> {{ $text }} </button>
 </div>
