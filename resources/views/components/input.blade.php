e054acc19a464c22f214fe24e7f6a4f60a9a0123
Pavel Vašíček <mr.troler@seznam.cz>
2021-01-26 16:26:28 +0100

    Changed debounce rate

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -12,7 +12,7 @@
         bg-transparent
     @endif
     focus:outline-none p-2 placeholder-gray-700 w-full xl:w-2/3 h-1/2 xl:h-full text-xl xl:text-3xl tracking-widest" 
-    placeholder="{{ $placeholder }}" type="{{ $type }}" wire:model.debounce.1000ms="{{ $for }}" name="{{ $for }}">
+    placeholder="{{ $placeholder }}" type="{{ $type }}" wire:model.debounce="{{ $for }}" name="{{ $for }}">
     @error($for)
         <p class="bg-red-700 text-white text-lg w-full xl:w-1/3 h-1/2 xl:h-full text-center flex items-center justify-center font-bold tracking-widest px-2"> {{ $errors->first($for) }} </p>
     @enderror
