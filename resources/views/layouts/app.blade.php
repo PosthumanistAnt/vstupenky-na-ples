bc932f9d5b6761bc98b47e0fe723e410b369f418
Pavel Vašíček <mr.troler@seznam.cz>
2021-01-29 19:56:10 +0100

    Installed fabric and linked it

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vstupenky na ples</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
 
     @livewireStyles
 </head>
 <body>
-    <div class="container h-screen w-screen ">
+    <div class="container">
         {{ $slot }}
     </div>
     
