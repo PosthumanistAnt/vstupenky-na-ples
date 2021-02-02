a5b0e9deb52b6d3f1fb309f53ba3e5dd663b945a
Pavel Vašíček <mr.troler@seznam.cz>
2021-02-02 12:05:51 +0100

    Modified SeatPicker

--- app/Http/Livewire/SeatPicker.php
+++ app/Http/Livewire/SeatPicker.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class SeatPicker extends Component
 {
+    public function admin()
+    {
+        return redirect('admin');
+    }
+
+    public function logout()
+    {
+        return redirect('logout');
+    }
     public function render()
     {
         return view('livewire.seat-picker');
--- resources/views/livewire/seat-picker.blade.php
+++ resources/views/livewire/seat-picker.blade.php
@@ -1,3 +1,33 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="h-full w-screen bg-gray-900 text-gray-300">
+    <div class="w-full xl:flex xl:justify-between">
+        <x-buttons.secondary-button text='Odhlásit' livewireFunction='logout' />
+        @admin
+            <x-buttons.secondary-button text='Admin' livewireFunction='admin' />
+        @endadmin
+    </div>
+    
+    <div id="c" class="w-screen h-screen">
+        <canvas height="1000" width="1000" id="canvas" class="w-screen h-screen"></canvas>
+    </div>
+
+    <script>
+
+        var canvas = new fabric.Canvas('canvas');
+        var rects =  [];
+        for (let i = 10; i < 78; i++) {
+            // create a rectangle
+            rects.push(
+                new fabric.Rect({
+                left: i%8 * 30,
+                top: parseInt(i/8) * 30,
+                fill: '#' + (i-10) + i + (i+20),
+                width: 20,
+                height: 20,
+            }));     
+        }
+
+        for (let index = 0; index < rects.length; index++) {
+            canvas.add(rects[index]);   
+        }
+    </script>
 </div>
