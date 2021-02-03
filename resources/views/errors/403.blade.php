4d3b118271877c3e4f91aec8e2b5b219b68dc703
Pavel Vašíček <mr.troler@seznam.cz>
2021-02-03 18:44:20 +0100

    💄 Created a custom 403 response page

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vstupenky na ples</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>    
+    <div class="fixed inset-x-0 top-1 text-center xl:flex xl:justify-between xl:items-baseline">
+        @auth
+            <a href="{{ url('/') }}">
+                <button class="p-6 text-5xl xl:text-8xl tracking-widest font-extrabold text-gray-300 hover:text-gray-400"> Vybrat vstupenky </button>
+            </a> 
+        @endauth   
+        @guest
+            <a href="{{ route('login') }}">
+                <button class="p-6 text-5xl xl:text-8xl tracking-widest font-extrabold text-gray-300 hover:text-gray-400"> Přihlásit </button>
+            </a> 
+        @endguest
+    </div>
+    <div class="container">
+        <div class="h-screen w-screen bg-gray-900 text-gray-300 flex justify-center items-center">
+            <h1 class="text-5xl font-bold tracking-wide p-8 w-full text-center">Nemáte dostatečná práva</h1>
+        </div>
+    </div>
+</body>
+</html>
