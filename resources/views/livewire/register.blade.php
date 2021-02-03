7351217ef1f85e26c75f81ced7228acad85f1c2c
Pavel Vašíček <mr.troler@seznam.cz>
2021-02-03 18:42:52 +0100

    Added a form tag

--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -1,12 +1,14 @@
 <div class="h-screen w-screen bg-gray-900 text-gray-300">
-    <div class="h-2/3 ">
-        <x-input for="name" height="1/4" type="text" placeholder="Jméno" />
-        <x-input for="email" height="1/4" type="email" placeholder="Email" />
-        <x-input for="password" height="1/4" type="password" placeholder="Heslo" />
-        <x-input for="password_confirmation" height="1/4" type="password" placeholder="Potvrzení hesla" />
-    </div>
-    <div class="fixed inset-x-0 bottom-1 text-center xl:flex xl:justify-between xl:items-baseline ">
-        <x-buttons.primary-button text='Registrovat' livewireFunction='register' />
-        <x-buttons.secondary-button text='Přihlášení' livewireFunction='login' />
-      </div>
+    <form class="h-full w-full" wire:submit.prevent>
+        <div class="h-2/3 ">
+            <x-input for="name" height="1/4" type="text" placeholder="Jméno" />
+            <x-input for="email" height="1/4" type="email" placeholder="Email" />
+            <x-input for="password" height="1/4" type="password" placeholder="Heslo" />
+            <x-input for="password_confirmation" height="1/4" type="password" placeholder="Potvrzení hesla" />
+        </div>
+        <div class="fixed inset-x-0 bottom-1 text-center xl:flex xl:justify-between xl:items-baseline ">
+            <x-buttons.primary-button text='Registrovat' livewireFunction='register' />
+            <x-buttons.secondary-button text='Přihlášení' livewireFunction='login' />
+          </div>
+    </form>
 </div>
