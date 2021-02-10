e31d5ac362712ef7dab2dbe3fa03682df5ef3b3f
Pavel Vašíček <peval321@gmail.com>
2021-02-10 22:47:48 +0100

    💄 Changed credentials error's height

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -4,7 +4,7 @@
       <x-input for='email' type='email' placeholder='Email' height='1/2' />
       <x-input for='password' type='password' placeholder='Heslo' height='1/2' />
     </div>
-    <div class="h-2/6 xl:h-3/6">
+    <div class="h-2/6">
       @if( $credentials_error )
         <div class="bg-red-700 text-white tracking-widest text-3xl xl:text-5xl font-bolder h-full w-full text-center flex items-center justify-center"> {{ $credentials_error }} </div>
       @endif  
