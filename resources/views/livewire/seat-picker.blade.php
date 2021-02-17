83e1289f70ec8b56c7f5cb22ae66b6ec36ae8d78
Pavel Vašíček <peval321@gmail.com>
2021-02-17 14:42:46 +0100

    Improved seat positioning

--- resources/views/livewire/seat-picker.blade.php
+++ resources/views/livewire/seat-picker.blade.php
@@ -30,17 +30,62 @@
         });
 
         var seats =  [];
-        var seatWidth = 20
+        var seatWidth = 20;
 
         var tables =  [];
         var tableWidth = 50;
+        
+        //change in position of seat depending on its index in table
+        var positionsList = [
+            {
+                corner: 'tl',
+                x: 0,
+                y: -1
+            },
+            {
+                corner: 'tr',
+                x: -1,
+                y: -1
+            },
+            {
+                corner: 'bl',
+                x: 0,
+                y: 0
+            },
+            {
+                corner: 'br',
+                x: -1,
+                y: 0
+            },
+            {
+                corner: 'tl',
+                x: -1,
+                y: 0
+            },
+            {
+                corner: 'bl',
+                x: -1,
+                y: -1
+            },
+            {
+                corner: 'tr',
+                x: 0,
+                y: 0
+            },
+            {
+                corner: 'br',
+                x: 0,
+                y: -1
+            }
+        ];
 
+        //set js objects from results from DB
         @foreach ($tables as $table)
             @foreach ($table->seats as $seat)
                 seats.push(
                     new fabric.Rect({
-                        left: {{ $table->position_x }} - seatWidth,
-                        top: {{ $table->position_y }} - seatWidth,
+                        left: {{ $table->position_x }},
+                        top: {{ $table->position_y }},
                         width: seatWidth,
                         height: seatWidth,
                         fill: "blue",
@@ -62,32 +107,32 @@
             seats = [];
         @endforeach
                     
+        //add everything to canvas
         tables.forEach(function(table) {
             canvas.add(table);  
+            var seatsLength = table.seats.length;
+
             table.seats.forEach(function(seat, seatIndex) {
-                canvas.add(seat);  
+                evaluatedPosition = evaluateSeatPosition(seatIndex, table);
+                seat.left = evaluatedPosition.x;
+                seat.top = evaluatedPosition.y;
+                // console.log(seat.top);
+                canvas.add(seat); 
             });
         });
-        for (let i = 0; i < 2; i++) {
-            for (let j = 0; j < 2; j++) {
-            
-            }
-        }
 
-        function evaluateX(index) {
-            if (index < 4) {
-                return (index % 2) + 1;
-            } else {
-                return (index % 4 > 1) * 3 
-            }
-        }
 
-        function evaluateY(index) {
-            if (index < 4) {
-                return (index % 4 > 1) * 3 
-            } else {
-                return (index % 2) + 1;
-            }
+        function evaluateSeatPosition(seatIndex, table) {
+            var position = this.positionsList[seatIndex];
+            var tableCoords = table.aCoords;
+            console.log(typeof(tableCoords[position.corner].x));
+            console.log(tableCoords[position.corner].x);
+            var x = tableCoords[position.corner].x + position.x * seatWidth;
+            var y = tableCoords[position.corner].y + position.y * seatWidth;
+            return {
+                x: x,
+                y: y
+            };
         }
     </script>
 </div>
