e6b9c6f45d75e7f1fd74035ba764c3901c89095c
Pavel Vašíček <peval321@gmail.com>
2021-02-25 22:59:46 +0100

    ✨ ✨ ✨ well this is a fucking mess Tables can be moved around now Move stuff around to be more readable Renamed variables to be readable Added stickman :)

--- resources/views/livewire/seat-picker.blade.php
+++ resources/views/livewire/seat-picker.blade.php
@@ -15,68 +15,13 @@
     </div>
 
     <script>   
-        // resize the canvas  
-        var canvas = new fabric.Canvas('canvas', {
-            backgroundColor: 'brown',
-            width: document.getElementById('canvas-wrapper').clientWidth,
-            height: document.getElementById('canvas-wrapper').clientHeight,
-        });
-
-        //panning and zooming
-        canvas.on('mouse:down', function(opt) {
-            var evt = opt.e;
-            if (evt.altKey === true) {
-                this.isDragging = true;
-                this.selection = false;
-                this.lastPosX = evt.clientX;
-                this.lastPosY = evt.clientY;
-            }
-        });
-
-        canvas.on('mouse:move', function(opt) {
-            if (this.isDragging) {
-                var e = opt.e;
-                var vpt = this.viewportTransform;
-                vpt[4] += e.clientX - this.lastPosX;
-                vpt[5] += e.clientY - this.lastPosY;
-                this.requestRenderAll();
-                this.lastPosX = e.clientX;
-                this.lastPosY = e.clientY;
-            }
-        });
-
-        canvas.on('mouse:up', function(opt) {
-            // on mouse up we want to recalculate new interaction
-            // for all objects, so we call setViewportTransform
-            this.setViewportTransform(this.viewportTransform);
-            this.isDragging = false;
-            this.selection = true;
-        });
-
-        canvas.on('mouse:down', function(opt) {
-            if(opt.target && opt.target.seatId !== undefined){
-                var seatId = opt.target.seatId;
-                Livewire.emit('addToCart', seatId);
-            }
-        });
-
-        canvas.on('mouse:wheel', function(opt) {
-        var delta = opt.e.deltaY;
-        var zoom = canvas.getZoom();
-        zoom *= 0.98 ** delta;
-        if (zoom > 5) zoom = 5;
-        if (zoom < 0.2) zoom = 0.2;
-        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-        opt.e.preventDefault();
-        opt.e.stopPropagation();
-        });
         var seats =  [];
         var seatWidth = 20;
 
         var tables =  [];
         var tableWidth = 50;
         
-        //change in position of seat depending on its index in table
+        // change in position of seat depending on its index in table
         var positionsList = [
             {
                 corner: 'tl',
@@ -120,59 +65,275 @@
             }
         ];
 
-        //push results from DB to js arrays
-        @foreach ($tables as $table)
-            @foreach ($table->seats as $seat)
-                seats.push(
-                    new fabric.Rect({
-                        left: {{ $table->position_x }},
-                        top: {{ $table->position_y }},
-                        width: seatWidth,
-                        height: seatWidth,
-                        fill: "blue",
-                        seatId: {{ $seat->id }},
-                        seatType: {{ $seat->seatType->id }},
-                    })
-                );   
-            @endforeach
-            tables.push(
-                new fabric.Rect({
-                    left: {{ $table->position_x }},
-                    top: {{ $table->position_y }},
-                    width: tableWidth,
-                    height: tableWidth,
-                    fill: "black",
-                    seats: seats,
-                })
-            );
-            seats = [];
-        @endforeach
-                    
-        //add everything to canvas
-        tables.forEach(function(table) {
-            canvas.add(table);  
-            var seatsLength = table.seats.length;
-
-            table.seats.forEach(function(seat, seatIndex) {
-                evaluatedPosition = evaluateSeatPosition(seatIndex, table, positionsList);
-                seat.left = evaluatedPosition.x;
-                seat.top = evaluatedPosition.y;
-                canvas.add(seat); 
-            });
+        // resize and initialize the canvas  
+        var canvas = new fabric.Canvas('canvas', {
+            backgroundColor: 'brown',
+            width: document.getElementById('canvas-wrapper').clientWidth,
+            height: document.getElementById('canvas-wrapper').clientHeight,
         });
 
+        // panning
+        canvas.on( 'mouse:down', function(opt) {
+            var evt = opt.e;
+            if (evt.altKey === true) {
+                this.isDragging = true;
+                this.selection = false;
+                this.lastPosX = evt.clientX;
+                this.lastPosY = evt.clientY;
+            }
+        });
 
-        function evaluateSeatPosition(seatIndex, table, positionsList) {
-            var position = positionsList[seatIndex];
+        // still panning
+        canvas.on( 'mouse:move', function(opt) {
+            if (this.isDragging) {
+                var e = opt.e;
+                var vpt = this.viewportTransform;
+                vpt[4] += e.clientX - this.lastPosX;
+                vpt[5] += e.clientY - this.lastPosY;
+                this.requestRenderAll();
+                this.lastPosX = e.clientX;
+                this.lastPosY = e.clientY;
+            }
+        });
+
+        // also panning
+        canvas.on( 'mouse:up', function(opt) {
+            // on mouse up we want to recalculate new interaction
+            // for all objects, so we call setViewportTransform
+            this.setViewportTransform(this.viewportTransform);
+            this.isDragging = false;
+            this.selection = true;
+        });
+        
+        // zooming
+        canvas.on( 'mouse:wheel', function(opt) {
+            var delta = opt.e.deltaY;
+            var zoom = canvas.getZoom();
+            zoom *= 0.98 ** delta;
+            if (zoom > 5) zoom = 5;
+            if (zoom < 0.2) zoom = 0.2;
+            canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+            opt.e.preventDefault();
+            opt.e.stopPropagation();
+        });
+        
+        // clicking on a seat and adding it to cart
+        canvas.on( 'mouse:down', function(opt) {
+            if( opt.target && opt.target.seatId !== undefined ){
+                var seat = opt.target;
+                seat.set("fill", 'green');
+            } 
+        });
+
+        // figuring out how groups work
+        canvas.on( 'mouse:down', function(opt) {
+            var table = new fabric.Rect ({
+                width: 100,
+                height: 20,
+                fill: '#eef',
+                scaleY: 1,
+                originX: 'center',
+                originY: 'center',
+                seats: [],
+            })
+
+            table.seats.push( new fabric.Circle ({
+                radius: 100,
+                fill: '#eef',
+                scaleY: 0.5,
+                originX: 'center',
+                originY: 'center'
+            }));
+
+            table.seats.push( new fabric.Circle ({
+                radius: 40,
+                fill: '#eef',
+                scaleY: 2,
+                originX: 'left',
+                originY: 'top'
+            }));
+
+            var group = new fabric.Group( [ table ] , {
+                left: 150,
+                top: 100,
+                angle: -10
+            });
+            
+            group.addWithUpdate( new fabric.Circle ({
+                radius: 40,
+                fill: '#eef',
+                scaleY: 2,
+                originX: 'center',
+                originY: 'center',
+            }));
+            
+            // var group = new fabric.Group([ circle, text ], {
+            //     left: 150,
+            //     top: 100,
+            //     angle: -10
+            // });
+
+            //canvas.add(group);
+        });
+        
+        // push results from DB to js arrays
+        @foreach ( $tables as $table )
+            // var table = new fabric.Rect({
+            //     left: {{ $table->position_x }}+20,
+            //     top: {{ $table->position_y }}+20,
+            //     width: tableWidth,
+            //     height: tableWidth,
+            //     fill: "blue",
+            // });
+            
+            // canvas.add(table);
+
+            var table = new fabric.Rect({
+                left: {{ $table->position_x }},
+                top: {{ $table->position_y }},
+                width: tableWidth,
+                height: tableWidth,
+                fill: "black",
+                label: {{ $table->id }},
+                seats: [],
+            });
+
+
+            @foreach( $table->seats as $seat )
+                table.seats.push( new fabric.Rect({
+                    left: table.left+50,
+                    // left: evaluateSeatPosition( {{ $loop->index }},  table),
+                    top: table.top+50,
+                    width: seatWidth,
+                    height: seatWidth,
+                    fill: "blue",
+                    label: {{ $seat->id }},
+                    selectable: false,
+                    seatId: {{ $seat->id }},
+                    seatType: {{ $seat->seatType->id }},
+                }));  
+                canvas.add( table.seats[ table.seats.length - 1 ]);
+            @endforeach
+
+
+            canvas.add(table);
+        @endforeach
+
+
+                    
+        canvas.on('object:moving', function(e) {
+            var table = e.target;
+
+            table.seats.forEach( function( seat, seatIndex ) {
+                var seatPosition = evaluateSeatPosition(seatIndex, table)
+                seat.left = seatPosition.x;
+                seat.top = seatPosition.y;
+            });
+
+            canvas.on('object:moved', function(e) {
+                var table = e.target;
+
+                table.seats.forEach( function( seat, seatIndex ) {
+                    var seatPosition = evaluateSeatPosition(seatIndex, table)
+                    seat.left = seatPosition.x;
+                    seat.top = seatPosition.y;
+                });
+            });
+            
+            // if( opt.target && opt.target.seatId !== undefined ){
+            // }
+            var p = e.target;
+            p.line1 && p.line1.set({ 'x2': p.left, 'y2': p.top });
+            p.line2 && p.line2.set({ 'x1': p.left, 'y1': p.top });
+            p.line3 && p.line3.set({ 'x1': p.left, 'y1': p.top });
+            p.line4 && p.line4.set({ 'x1': p.left, 'y1': p.top });
+            canvas.renderAll();
+        });
+        // add everything to canvas
+        // tables.forEach(function(table) {
+
+        //     canvas.add(table);  
+        //     var seatsLength = table.seats.length;
+
+        //     table.seats.forEach(function(seat, seatIndex) {
+        //         evaluatedPosition = evaluateSeatPosition(seatIndex, table);
+        //         seat.left = evaluatedPosition.x;
+        //         seat.top = evaluatedPosition.y;
+        //         canvas.add(seat); 
+        //     });
+        // });
+
+
+        function evaluateSeatPosition(seatIndex, table) {
+            var desiredPosition = positionsList[seatIndex];
             var tableCoords = table.aCoords;
 
-            var x = tableCoords[position.corner].x + position.x * seatWidth;
-            var y = tableCoords[position.corner].y + position.y * seatWidth;
+            var x = tableCoords[desiredPosition.corner].x + desiredPosition.x * seatWidth;
+            var y = tableCoords[desiredPosition.corner].y + desiredPosition.y * seatWidth;
 
             return {
                 x: x,
                 y: y
             };
         }
+
+
+    function makeCircle(left, top, line1, line2, line3, line4) {
+        var c = new fabric.Circle({
+        left: left,
+        top: top,
+        strokeWidth: 5,
+        radius: 12,
+        fill: '#fff',
+        stroke: '#666'
+        });
+        c.hasControls = c.hasBorders = false;
+
+        c.line1 = line1;
+        c.line2 = line2;
+        c.line3 = line3;
+        c.line4 = line4;
+
+        return c;
+    }
+
+    function makeLine(coords) {
+        return new fabric.Line(coords, {
+        fill: 'red',
+        stroke: 'red',
+        strokeWidth: 5,
+        selectable: false,
+        evented: false,
+        });
+    }
+
+    var line = makeLine([ 250, 125, 250, 175 ]),
+        line2 = makeLine([ 250, 175, 250, 250 ]),
+        line3 = makeLine([ 250, 250, 300, 350]),
+        line4 = makeLine([ 250, 250, 200, 350]),
+        line5 = makeLine([ 250, 175, 175, 225 ]),
+        line6 = makeLine([ 250, 175, 325, 225 ]);
+
+    canvas.add(line, line2, line3, line4, line5, line6);
+
+    canvas.add(
+        makeCircle(line.get('x1'), line.get('y1'), null, line),
+        makeCircle(line.get('x2'), line.get('y2'), line, line2, line5, line6),
+        makeCircle(line2.get('x2'), line2.get('y2'), line2, line3, line4),
+        makeCircle(line3.get('x2'), line3.get('y2'), line3),
+        makeCircle(line4.get('x2'), line4.get('y2'), line4),
+        makeCircle(line5.get('x2'), line5.get('y2'), line5),
+        makeCircle(line6.get('x2'), line6.get('y2'), line6)
+    );
+
+    canvas.on('object:moving', function(e) {
+        var p = e.target;
+        p.line1 && p.line1.set({ 'x2': p.left, 'y2': p.top });
+        p.line2 && p.line2.set({ 'x1': p.left, 'y1': p.top });
+        p.line3 && p.line3.set({ 'x1': p.left, 'y1': p.top });
+        p.line4 && p.line4.set({ 'x1': p.left, 'y1': p.top });
+        canvas.renderAll();
+    });
+
     </script>
 </div>
