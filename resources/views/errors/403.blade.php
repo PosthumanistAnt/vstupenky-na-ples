09c7094d9c7508803b6d1cff4926674173a0b6ef
Pavel Vašíček <peval321@gmail.com>
2021-04-05 16:02:54 +0200

    Move button to bottom

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,7 +3,7 @@
         <h1 class="text-5xl font-bold tracking-wide p-8 w-full text-center">Nemáte dostatečná práva</h1>
     </div>
 
-    <div class="fixed inset-x-0 top-1 text-center xl:flex xl:justify-between xl:items-baseline">
+    <div class="fixed inset-x-0 bottom-1 text-center xl:flex xl:justify-between xl:items-baseline">
         @auth
             <a href="{{ url('/') }}">
                 <button class="btn btn-primary"> Vybrat vstupenky </button>
