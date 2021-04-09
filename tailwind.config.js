8f19c7a80ba5105c297313ca116d3cdb74543864
Pavel Vašíček <peval321@gmail.com>
2021-04-09 15:51:59 +0200

    Add 80vh and 60vh variants to height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {    
+      height: {
+        "screen-3/5": '60vh',
+        "screen-4/5": '80vh',
+      }
+    },
   },
   variants: {
     extend: {},
