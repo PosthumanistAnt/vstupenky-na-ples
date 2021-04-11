0394ea843eae1bf2a76112b525933ec695826366
Pavel Vašíček <peval321@gmail.com>
2021-04-11 16:46:16 +0200

    Add modal with canvas controls

--- resources/views/livewire/seat-picker.blade.php
+++ resources/views/livewire/seat-picker.blade.php
@@ -3,8 +3,33 @@
         <div id="canvas-wrapper" class="w-full xl:w-2/3 h-full" wire:ignore>
             <canvas id="canvas"></canvas>
         </div>
-        <div class="w-full xl:w-1/3 h-full mt-4 xl:mt-0">
-            <h2 class="text-3xl text-center tracking-wide mb-2"> Nákupní košík </h2>
+        <div class="w-full xl:w-1/3 h-full xl:mt-0 text-center ">
+            <div class="mt-6" x-data="{ open: false }">
+                <button class="p-4 bg-gray-800 hover:bg-gray-700" @click="open = true">Zobrazit ovládání aplikace</button>
+                <div class="absolute top-0 left-0 flex items-center justify-center w-full h-full" style="background-color: rgba(0,0,0,.5);" x-show="open">
+                    <div class="h-auto p-4 mx-2 text-left bg-gray-900 text-white shadow-xl md:max-w-xl md:p-6 lg:p-8 md:mx-0" @click.away="open = false">
+                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <h3 class="text-lg font-medium leading-6">
+                                Ovládání aplikace
+                            </h3>
+                
+                            <div class="mt-2">
+                                <p class="text-sm leading-5 text-gray-300">
+                                Kolekčem od myši se oddaluje a přibližuje. Podržením klávesy alt a zároveň tažením myši se mění pozice. Kliknutím na požadovánou vstupenku se vloží do košíku.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="mt-5 sm:mt-6">
+                            <span class="flex w-full rounded-md shadow-sm">
+                                <button @click="open = false" class="inline-flex justify-center w-full p-4 text-white bg-gray-800 hover:bg-gray-700">
+                                Zavřít
+                                </button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h2 class="text-3xl text-center tracking-wide my-2"> Nákupní košík </h2>
             <div class="overflow-y-scroll h-2/3 border-gray-800 border-b-2">
             <table class="w-full mt-4">
                 <tr>
@@ -16,14 +41,14 @@
                     <tr class="px-8 border-gray-800 border-b">
                         <td class="text-center"> {{ $selectedSeat->number }} </td>
                         <td class="text-center"> {{ $selectedSeat->seatType->price }} </td>
-                        <td class="text-center"> <button class="p-2 px-4 m-2 bg-gray-800" wire:click="$emit('seatDeselected', '{{ $selectedSeat->id }}' )"> X </button> </td>
+                        <td class="text-center"> <button class="p-2 px-4 m-2 bg-gray-800 hover:bg-gray-700" wire:click="$emit('seatDeselected', '{{ $selectedSeat->id }}' )"> X </button> </td>
                     </tr>
                 @endforeach
             </table>
             </div>
             <div class="flex items-center justify-evenly mt-8">
                 <p class="text-center text-xl"> Celková cena: {{ $totalPrice }} </p>
-                <button class="p-4 bg-gray-800" wire:click='orderSeats'> Objednat </button>
+                <button class="p-4 bg-gray-800 hover:bg-gray-700" wire:click='orderSeats'> Objednat </button>
             </div>
         </div>
     </div>
@@ -31,7 +56,7 @@
         <div class="fixed bottom-36 w-full h-16">
             <div class="bg-blue-400 text-black text-3xl flex justify-evenly items-center h-full">
                 <p> {{ session('order_verified') }} </p>
-                <button class="p-2 px-8 bg-gray-800 text-white" wire:click="unsetMessage('order_verified')"> X </button>
+                <button class="p-2 px-8 bg-gray-800 hover:bg-gray-700 text-white" wire:click="unsetMessage('order_verified')"> X </button>
             </div>
         </div>
     @endif
@@ -39,7 +64,7 @@
         <div class="fixed bottom-4 w-full h-16">
             <div class="bg-yellow-400 text-black text-3xl flex justify-evenly items-center h-full">
                 <p> {{ session('cart_empty') }} </p>
-                <button class="p-2 px-8 bg-gray-800 text-white" wire:click="unsetMessage('cart_empty')"> X </button>
+                <button class="p-2 px-8 bg-gray-800 hover:bg-gray-700 text-white" wire:click="unsetMessage('cart_empty')"> X </button>
             </div>
         </div>
     @endif
@@ -47,10 +72,11 @@
         <div class="fixed bottom-20 w-full h-16">
             <div class="bg-blue-400 text-black text-3xl flex justify-evenly items-center h-full">
                 <p> {{ session('order_placed') }} </p>
-                <button class="p-2 px-8 bg-gray-800 text-white" wire:click="unsetMessage('order_placed')"> X </button>
+                <button class="p-2 px-8 bg-gray-800 hover:bg-gray-700 text-white" wire:click="unsetMessage('order_placed')"> X </button>
             </div>
         </div>
     @endif
+
     <script>
         var seatWidth = 20;
 
