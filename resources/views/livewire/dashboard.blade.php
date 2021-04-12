2976c06651bbe001c3693ec79a5b580ccb794b01
Pavel Vašíček <peval321@gmail.com>
2021-04-12 11:46:03 +0200

    Fix url in anchor

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,15 +1,4 @@
 <div class="w-screen mt-40 h-screen">
-    {{-- <table class="my-8">
-        <tr class="bg-blue-400">
-        <td class="p-2">STUFF</td>
-        </tr>
-        <tr class="h-4">
-        <td></td>
-        </tr>
-        <tr class="bg-blue-400">
-        <td class="p-2">MORE STUFF</td>
-        </tr>
-        </table> --}}
     <div class="w-full h-full">
         <table class="w-full p-8">
             <tr >
@@ -22,7 +11,7 @@
                     <td class="text-center p-4"> {{ $order->orderItems->sum('seat.seatType.price') }} </td>
                     <td class="text-center p-4"> {{ __('order_states.' . $order->state->state) }} </td>
                     <td class="text-center p-4"> {{ $order->created_at->format('H:i:s d. m. Y') }} </td>
-                    <td class="text-center"> <a href="{{ url('order ' . $order->id) }}" class="p-4 bg-gray-800 hover:bg-gray-700"> Podrobnosti  </a> </td>
+                    <td class="text-center"> <a href="{{ route('order', ['id' => $order->id]) }}" class="p-4 bg-gray-800 hover:bg-gray-700"> Podrobnosti  </a> </td>
                 </tr>
             @endforeach
         </table>
