952451d9970a4548eb630bed984405c0fc20912b
Pavel Vašíček <peval321@gmail.com>
2021-04-12 08:49:29 +0200

    Add translation files for order states

---
+++ resources/lang/cs/order_states.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Order states
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'accepted'   => 'Potvrzeno',
+    'declined' => 'Zamítnuto',
+    'pending' => 'Čeká na potvrzení',
+
+];
