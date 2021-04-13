11d91c12bafddf5961079322d7f810973cb6dde4
Pavel Vašíček <peval321@gmail.com>
2021-04-13 16:38:20 +0200

    Add plain text verification link

--- resources/views/emails/reservation/confirmation.blade.php
+++ resources/views/emails/reservation/confirmation.blade.php
@@ -1,2 +1,3 @@
 <p> Pro dokončení rezervace vstupenek klikněte na tlačítko níže. </p>
 <a href="{{ $url }}"> Dokončit rezervaci </a>
+<p> V případě, že vám nefunguje odkaz na dokončení, zkopírujte si do adresního řádku {{ $url }} </p>
