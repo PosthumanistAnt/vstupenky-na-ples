ffb0dbe76b4574315048a3b7d5271aa6ffab17d3
Pavel Vašíček <peval321@gmail.com>
2021-04-15 13:20:14 +0200

    Translate admin panel

---
+++ resources/lang/vendor/lean/cs/resources.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'index' => [
+        // Top section
+        'search' => 'Vyhledat',
+        'new' => 'Přidat :Resource',
+
+        // Buttons
+        'show' => 'Zobrazit',
+        'edit' => 'Upravit',
+        'delete' => 'Smazat',
+
+        'pagination' => [
+            'previous' => '&laquo; Předchozí',
+            'next' => 'Další &raquo;',
+            'goto' => 'Přejít na stránku :page',
+            'results' => 'Zobrazeno :start až :end z :total výsledků',
+        ],
+        'no_results' => 'Žádné výsledky.',
+    ],
+
+    'create' => [
+        'title' => 'Vytvořit :Resource',
+        'submit' => 'Vytvořit :Resource',
+        'another' => 'Vytvořit a přidat další :Resource',
+        'edit' => 'Vytvořit a přejít na editaci :Resource',
+    ],
+
+    'show' => [
+        'title' => ':Resource :title',
+        'delete' => 'Smazat',
+        'delete_confirm' => 'Opravdu chcete smazat :resource?',
+        'edit' => 'Upravit',
+    ],
+
+    'edit' => [
+        'title' => 'Upravit :Resource :title',
+        'submit' => 'Uložit :Resource',
+        'submit_mobile' => 'Uložit', // Short version for mobile screens
+    ],
+
+    'back' => 'Zpět',
+
+    'notifications' => [
+        'created' => 'Záznam :Resource vytvořen!',
+        'updated' => 'Záznam :Resource upraven!',
+        'deleted' => 'Záznam :Resource vymazán!',
+    ],
+];
